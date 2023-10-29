85e3c41848937e2e25de99dd6185cd860f518838
7jjin <wlsgud6305@naver.com>
2023-10-29 12:23:41 +0900

    prettier 설치 및 서로소 타입

--- section1/dist/index.js
+++ section1/dist/index.js
@@ -1,4 +1,5 @@
 const a = 1;
 export {};
+// typescript 파일은 모두 전역 모듈로 인식하고 있다.
 // export,import를 한번이라도 사용하면 그 공간은 독립된 공간으로 생각한다.
 // export {};
---
+++ section3/src/chapter8.ts
@@ -0,0 +1,108 @@
+/**
+ * 서로소 유니온 타입
+ * 교집합이 없는 타입들로만 만든 유니온 타입을 말함.
+ */
+
+type Admin = {
+  tag: "ADMIN";
+  name: string;
+  kickCount: number;
+};
+
+type Member = {
+  tag: "MEMBER";
+  name: string;
+  point: number;
+};
+
+type Guest = {
+  tag: "GUEST";
+  name: string;
+  visitCount: number;
+};
+
+// tag에 string 리터럴 타입을 넣음으로써 교집합이 없어서 고유의 값이 된다.
+
+type User = Admin | Member | Guest;
+
+// Admin -> {name}님 현재까지 {kickCount}명 강퇴했습니다.
+// Member -> {name}님 현재까지 {point}모았습니다.
+// Guest -> {name}님 현재까지 {visitCount}번 오셨습니다.
+function login(user: User) {
+  switch (user.tag) {
+    case "ADMIN": {
+      console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
+      break;
+    }
+    case "MEMBER": {
+      console.log(`${user.name}님 현재까지 ${user.point}모았습니다.`);
+      break;
+    }
+    case "GUEST": {
+      console.log(`${user.name}님 현재까지 ${user.visitCount}번 방문하셨습니다.`);
+    }
+  }
+}
+
+/**
+ * 복습겸 한가지 더 사례
+ */
+
+// 비동기 작업의 결과를 처리하는 객체
+
+type LoadingTask = {
+  state: "LOADING";
+};
+
+type FailedTask = {
+  state: "FAILED";
+  error: {
+    message: string;
+  };
+};
+
+type SuccessTask = {
+  state: "SUCCESS";
+  response: {
+    data: string;
+  };
+};
+
+type AsyncTask = LoadingTask | FailedTask | SuccessTask;
+
+// 로딩 중 -> 콘솔에 로딩중 출력
+// 실패 -> 실패: 어레메세지를 출력
+// 성공 -> 성공: 데이터로 출력
+function processResult(task: AsyncTask) {
+  switch (task.state) {
+    case "LOADING": {
+      console.log("로딩중");
+      break;
+    }
+    case "FAILED": {
+      console.log(`에러 발생 : ${task.error.message}`);
+      break;
+    }
+    case "SUCCESS":
+      console.log(`성공 : ${task.response.data}`);
+      break;
+  }
+}
+
+const loading: AsyncTask = {
+  state: "LOADING",
+};
+
+const failed: AsyncTask = {
+  state: "FAILED",
+  error: {
+    message: "오류 발생 원인은 ~~",
+  },
+};
+
+const success: AsyncTask = {
+  state: "SUCCESS",
+  response: {
+    data: "데이터~~",
+  },
+};
