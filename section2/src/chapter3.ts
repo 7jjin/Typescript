386111195f30118e3a905b213586fbf082d37228
7jjin <wlsgud6305@naver.com>
2023-10-30 17:37:28 +0900

    revise

--- section2/src/chapter3.ts
+++ section2/src/chapter3.ts
@@ -1,31 +1,27 @@
-
 // object
-let user:{
-    id?:number,     // ?표의 의미: 있을 수도 있고 없을 수도있다.(선택적 프로퍼티)
-    name:string
+let user: {
+  id?: number; // ?표의 의미: 있을 수도 있고 없을 수도있다.(선택적 프로퍼티)
+  name: string;
 } = {
-    id:1,
-    name:"조진형"
-}
+  id: 1,
+  name: "조진형",
+};
 
 // 객체 리터럴 타입
-let dog:{
-    name:string,
-    color:string
+let dog: {
+  name: "돌돌이";
+  color: string;
 } = {
-    name:"돌돌이",
-    color:"brown",
-}
+  name: "돌돌이",
+  color: "brown",
+};
 
 user = {
-    name:"홍길동"
-}
+  name: "홍길동",
+};
 
-let config:{
-    readonly apikey:string  // readonly = 값이 절때 바뀌면 안되는 프로퍼티에 추가
+let config: {
+  readonly apikey: string; // readonly = 값이 절때 바뀌면 안되는 프로퍼티에 추가
 } = {
-    apikey:"MY API KEY",
-}
-
-
-
+  apikey: "MY API KEY",
+};
--- section2/src/chapter6.ts
+++ section2/src/chapter6.ts
@@ -1,6 +1,6 @@
 // any
 // 특정 변수의 타입을 우리가 확실히 모를때
-let anyVar:any = 10;
+let anyVar: any = 10;
 anyVar = "hello";
 anyVar = false;
 anyVar = {};
@@ -8,21 +8,24 @@ anyVar = () => {};
 
 anyVar.toUpperCase();
 anyVar.toFixed();
+anyVar;
 // 런타임 때 오류가 발생함. anyVar는 마지막에 함수로 정의 되었지만 검사를 다 통과하고 런타임에 오류가 발생함. 따라서 typescript의 이점을 다 포기하는 것임
 
 let num: number = 10;
-num= anyVar;
-
+// if (typeof anyVar === "number") {
+//     num = anyVar;
+//   }
+num = anyVar;
 
 // unknow
 let unKnownVar: unknown;
 unKnownVar = "";
 unKnownVar = 1;
-unKnownVar = () =>{};
+unKnownVar = () => {};
 
-//unKnownVar.toUpperCase(); // 실해되지 않음
+//unKnownVar.toUpperCase(); // 실행되지 않음
 
 // 정제해서 넣을 수 있음
-if(typeof unKnownVar === "number"){
-    num = unKnownVar;
+if (typeof unKnownVar === "number") {
+  unKnownVar * 2;
 }
